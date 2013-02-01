2bb26ac2f0edcceb125a3f88a541a1afa3da14bf
Luis <luanur@hotmail.com>
2013-02-01 14:15:04 -0500

    fixin pol

--- app/controllers/respuestapol_controller.rb
+++ app/controllers/respuestapol_controller.rb
@@ -1,7 +1,13 @@
 class RespuestapolController < ApplicationController
   def index
-    if params[].present?
-      @usuario_id=params[:usuario_id]
+    if params[:usuario_id]==POLID
+      @fecha_procesamiento=params[:fecha_procesamiento]
+      @estadoTx= params[:estado]
+      @ref_venta= params[:ref_venta]
+      @ref_pol= params[:ref_pol]
+      @valor= params[:valor]
+      @moneda= params[:moneda]
+      @extra1= params[:extra1]
     end
   end
 end
--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -3,7 +3,7 @@ class WelcomeController < ApplicationController
   layout 'welcome'
 
   def index
-    @featured_product = Product.featured
+    @featured_product = Product.featured.active
     @best_selling_products = Product.limit(5)
     @other_products  ## search 2 or 3 categories (maybe based on the user)
     unless @featured_product
