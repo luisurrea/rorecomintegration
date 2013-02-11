d4105da213907bf9a21b8d711c5bafc06dda05a7
Luis <luanur@hotmail.com>
2013-02-11 10:52:47 -0500

    updating pol

--- app/controllers/respuestapol_controller.rb
+++ app/controllers/respuestapol_controller.rb
@@ -1,13 +1,7 @@
 class RespuestapolController < ApplicationController
   def index
-    if params[:usuario_id]==POLID
-      @fecha_procesamiento=params[:fecha_procesamiento]
-      @estadoTx= params[:estado]
-      @ref_venta= params[:ref_venta]
-      @ref_pol= params[:ref_pol]
-      @valor= params[:valor]
-      @moneda= params[:moneda]
-      @extra1= params[:extra1]
-    end
+   if params.nil?
+     redirect_to root
+   end
   end
 end
