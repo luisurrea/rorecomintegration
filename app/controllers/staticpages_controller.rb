3d1cf0c9538029630333cc7b879aeceb6690602b
Luis <luanur@hotmail.com>
2013-03-04 10:40:57 -0500

    updating changes

--- app/controllers/staticpages_controller.rb
+++ app/controllers/staticpages_controller.rb
@@ -1,16 +1,13 @@
 class StaticpagesController < ApplicationController
   def home
   end
-
   def empresa
-    
-    @page = Staticpage.where('name = ?', 'Empresa')
+     @page = Staticpage.where('name = ?', 'Empresa')
   end
-
   def terms
+    @page = Staticpage.where('name = ?', 'Terminos y Condiciones')
   end
-   
-   def conozca_tramontina
-     
-   end
+  def conozca_tramontina
+    @page = Staticpage.where('name = ?', 'Conozca Tramontina')
+  end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Hadean::Application.configure do
   #config.cache_store = :redis_store
   # Don't care if the mailer can't send
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'inoxdobrasil.com.co' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   :address              => "mail.inoxdobrasil.com.co",
