56896fc741efb7dbd9d35b7365c75e60de2f8430
Luis <luanur@hotmail.com>
2013-05-15 18:38:49 -0500

    ajustando historial

--- app/controllers/polconfirmation_controller.rb
+++ app/controllers/polconfirmation_controller.rb
@@ -6,11 +6,9 @@ class PolconfirmationController < ApplicationController
         if @order.empty?
           redirect_to root_path      
           flash[:alert] = 'no existe la orden'
-        else 
-          if params[:firma]==Digest::MD5.hexdigest(POLKEY+"~"+POLID.to_s+"~"+@order.polid+"~"+@order.totalorder.to_s+"~"+POLMONEDA+"~"+params[:estado_pol])
-            @order.credited_amount = 12345
-            @order.save
-          end
+        else
+          redirect_to root_path  
+          flash[:alert] = @order.polid
         end
         
       else 
