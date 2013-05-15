20d2a409bce08d0cff240ef9ff258fdc4466cf49
Luis <luanur@hotmail.com>
2013-05-15 17:53:40 -0500

    ajustando historial

--- app/controllers/polconfirmation_controller.rb
+++ app/controllers/polconfirmation_controller.rb
@@ -5,6 +5,7 @@ class PolconfirmationController < ApplicationController
         @order = Order.where('polid = ? ', params[:refVenta])
         if @order.nil?
           redirect_to root        
+          flash[:alert] = 'no existe la orden'
         else 
           if params[:firma]==Digest::MD5.hexdigest(POLKEY+"~"+POLID.to_s+"~"+@order.polid+"~"+@order.totalorder.to_s+"~"+POLMONEDA+"~"+params[:estado_pol])
             @order.credited_amount = 12345
