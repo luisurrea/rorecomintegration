fa991f1966136f80538dc9746c8b28221b0d0503
Luis <luanur@hotmail.com>
2013-05-16 14:58:25 -0500

    ljlkj

--- app/controllers/polconfirmation_controller.rb
+++ app/controllers/polconfirmation_controller.rb
@@ -2,9 +2,19 @@ class PolconfirmationController < ApplicationController
   
   def index
       if params[:firma]
+        @order = Order.find_by_polid(params[:refVenta])
+        
            @order.credited_amount = 12345
            @order.save
+           
+           #flash[:alert] = 'las firmas coinciden'
+           #else
+             #flash[:alert] = "No coinciden"
         
+        
+        
+       
+        #flash[:alert] = 'no llego parametro de firma'
       end
       
   end
