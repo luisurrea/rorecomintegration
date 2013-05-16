27940f93fb19fbfa8191c7e411797b267507f37a
Luis <luanur@hotmail.com>
2013-05-15 23:18:42 -0500

    kll

--- app/controllers/polconfirmation_controller.rb
+++ app/controllers/polconfirmation_controller.rb
@@ -4,20 +4,20 @@ class PolconfirmationController < ApplicationController
       if params[:firma]
         @order = Order.find_by_polid(params[:refVenta])
         if @order.nil?
-          redirect_to root_path      
-          flash[:alert] = 'no existe la orden'
+          #flash[:alert] = 'no existe la orden'
         else
           if params[:firma]==Digest::MD5.hexdigest(POLKEY+"~"+POLID.to_s+"~"+@order.polid+"~"+@order.totalorder.to_s+"~"+POLMONEDA+"~"+params[:estado_pol])
-           redirect_to root_path      
-           flash[:alert] = 'las firmas coinciden'
-           else
-             flash[:alert] = "firma parametro: "+params[:firma]+"  firma guardada:"+Digest::MD5.hexdigest(POLKEY+"~"+POLID.to_s+"~"+@order.polid+"~"+order.totalorder.to_s+"~"+POLMONEDA+"~"+params[:estado_pol])
+           @order.credited_amount = 12345
+           @order.save
+           
+           #flash[:alert] = 'las firmas coinciden'
+           #else
+             #flash[:alert] = "No coinciden"
           end
         end
         
       else 
-        redirect_to root_path
-        flash[:alert] = 'no llego parametro de firma'
+        #flash[:alert] = 'no llego parametro de firma'
       end
       
   end
