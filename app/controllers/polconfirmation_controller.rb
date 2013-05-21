acd5fc97bdc88e33de28c23252d07c6a39ceca16
Luis <luanur@hotmail.com>
2013-05-21 17:28:02 -0500

    añlkjvlz

--- app/controllers/polconfirmation_controller.rb
+++ app/controllers/polconfirmation_controller.rb
@@ -2,9 +2,7 @@ class PolconfirmationController < ApplicationController
   
   def index
       if params[:ref_venta]
-        @order = Order.find_by_polid(params[:refVenta])
-          @order.polid = 'yes'
-          @order.save      
+        @order = Order.find_by_polid(params[:ref_venta])
         if params[:firma]==Digest::MD5.hexdigest(POLKEY+"~"+POLID.to_s+"~"+@order.polid+"~"+@order.totalorder.to_s+"~"+POLMONEDA+"~"+params[:estado_pol])
           @order.order_complete!
           @order.save
