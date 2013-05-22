1671ac9025f805176dfb7eb5be34346bd4f39ec8
Luis <luanur@hotmail.com>
2013-05-21 23:49:11 -0500

    clkmlakf

--- app/controllers/polconfirmation_controller.rb
+++ app/controllers/polconfirmation_controller.rb
@@ -3,8 +3,7 @@ class PolconfirmationController < ApplicationController
   def index
       if params[:ref_venta]
         @order = Order.find_by_polid(params[:ref_venta])
-        value=number_with_precision(@order.totalorder, :precision => 2)
-        @order.polid=POLKEY+"~"+POLID.to_s+"~"+@order.polid+"~"+value.to_s+"~"+POLMONEDA+"~"+params[:estado_pol]+"~"+params[:firma]
+        @order.polid=POLKEY+"~"+POLID.to_s+"~"+@order.polid+"~"+number_with_precision(@order.totalorder, :precision => 2).to_s+"~"+POLMONEDA+"~"+params[:estado_pol]+"~"+params[:firma]
         @order.save
         if params[:firma]==Digest::MD5.hexdigest(POLKEY+"~"+POLID.to_s+"~"+@order.polid+"~"+@order.totalorder.to_s+"~"+POLMONEDA+"~"+params[:estado_pol])
           @order.order_complete!
