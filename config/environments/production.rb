f11ed74fdc73532569eeb766f49b8aa1bd54ab42
Luis <luanur@hotmail.com>
2013-07-20 10:43:21 -0500

    faafgaf

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -11,15 +11,15 @@ Hadean::Application.configure do
   config.assets.compress = true
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
-#  config.assets.compile = false
+  config.assets.compile = false
 
   # Generate digests for assets URLs
   config.assets.digest = true
 
   # config.assets.precompile += %w( *.css *.js )
 
-#  config.assets.precompile += %w( *.js )
-#  config.assets.precompile += %w( *.css )
+  config.assets.precompile += %w( *.js )
+  config.assets.precompile += %w( *.css )
   
   # Full error reports are disabled and caching is turned on
   config.consider_all_requests_local       = false
